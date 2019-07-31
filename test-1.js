598dc00a581bb16979b831dc779b103ec897d78e
dyatra <53493988+dyatra@users.noreply.github.com>
2019-07-31 10:04:56 +0300

    Add files via upload

---
+++ test-1.js
@@ -0,0 +1,10 @@
+let es6 = `sir pe mai multe linii
+solutia ES6 cu backtick
+la afisare formatarea se pastreaza`;
+console.log(es6);
+let es5 = "sir pe mai multe linii " +
+"solutia ES5 cu concatenare " +
+"la afisare formatarea nu se pastreaza";
+console.log(es5);
+let afisare = "linia 1\nlinia doi";
+console.log(afisare);
